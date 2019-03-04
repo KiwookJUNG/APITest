b60ab22d2fe39ecdeb2d190ff347fc779b5abd09
정기욱 <rldnr56@me.com>
2019-03-04 16:13:40 +0900

    DispatchQueue.main.async() 구문은 해당 블록의 코드가 메인 스레드에서 비동기적으로 실행되도록 요청하는 역할.
    JSONSerializaition을 이용해 전달받은 값을 JSON 객체로 변환(NSDictionary로) 하고 각각의 값을 추출하여
    
    텍스트 뷰에 출력.

--- APITest/ViewController.swift
+++ APITest/ViewController.swift
@@ -66,6 +66,32 @@ class ViewController: UIViewController {
                 return
             }
             // 5-2 응답 처리 로직
+            // 1. 메인 스레드에서 비동기로 처리되도록 한다.
+            DispatchQueue.main.async() {
+                do {
+                    let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+                    
+                    guard let jsonObject = object else { return }
+                    
+                    // 2. JSON 결과값을 추출한다.
+                    let result = jsonObject["result"] as? String
+                    let timestamp = jsonObject["timestamp"] as? String
+                    let userId = jsonObject["userId"] as? String
+                    let name = jsonObject["name"] as? String
+                    
+                    // 3. 결과가 성공일 때에만 텍스트 뷰에 출력한다.
+                    if result == "SUCCESS" {
+                        self.responseView.text = "아이디 : \(userId!)" + "\n"
+                            + "이름 : \(name!)" + "\n"
+                            + "응답결과 : \(result!)" + "\n"
+                            + "응답시간 : \(timestamp!)" + "\n"
+                            + "요청방식 : x-www-form-urlencoded"
+                    }
+                } catch let e as NSError {
+                    print("An error has occured while parsing JSONObject : \(e.localizedDescription)")
+                }
+            }
+            
         }
         // 6. POST 전송
         task.resume()
