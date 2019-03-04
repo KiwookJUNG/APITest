94937f7eb6b12fba652fa4658863bf08c4552ff9
정기욱 <rldnr56@me.com>
2019-03-04 15:12:38 +0900

    버튼과 라벨을 삽입한다.
    버튼의 액션은
    
    URL 객체를 만들어 String(contentsOf:) 구문을 이용하여 API를 읽어오는 부분과
    
    레이블에 시간을 표시해주는 부분을 작성해준다.

--- APITest/ViewController.swift
+++ APITest/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet var currentTime: UILabel!
+    
+    @IBAction func callCurrentTime(_ sender: Any) {
+        
+        // 1. URL 설정 및 GET 방식으로 API 호출
+        do {
+            let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/currentTime")
+            
+            let response = try String(contentsOf: url!)
+            
+            // 2. 읽어온 값을 레이블에 표시
+            self.currentTime.text = response
+            self.currentTime.sizeToFit()
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
+    
+    
+    
 }
 
